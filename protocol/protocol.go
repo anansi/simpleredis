bd7c7bd097efddb12202687bd6584d48b3de1e0d
Julian Hulme <julian@arcadesoftware.io>
2020-10-13 20:03:31 +0200

    Dear Clog. byte is not a reserved word in Go. Noted.
    now looping over the string, and converting to bytes

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -2,9 +2,25 @@ package protocol
 
 import "fmt"
 
+
+
 // Encode returns the required binary data to send across our network call
 func Encode(message string) string {
-    // Return a greeting that embeds the name in a message.
+    
+    
+	
+	fmt.Println("about to Encode: ", message)
+
+	for i := 0; i < len(message); i++ {
+		fmt.Println(i, ": ", message[i], "(decimal)")
+
+		char := message[i]
+		byte := fmt.Sprintf("%08b", byte(char))
+		fmt.Println(i, ": ", byte, "(byte)")
+	}
+	
+
+
     result := fmt.Sprintf("Hi, %v. Welcome!", message)
     return result
 }
