6014e0da7f58323f9453d2109aa83888291f7df5
Julian Hulme <julian@arcadesoftware.io>
2020-10-13 19:00:50 +0200

    more involved conn request capturing

--- server/server.go
+++ server/server.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
 func handleConnection(conn net.Conn) {
-	fmt.Println("handling Connection")
+	fmt.Println("handling Connection", conn)
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		message := string(buffer[:n])
+
+		if message == "/quit" {
+			fmt.Println("quit command received. Bye.")
+			return
+		}
+
+		if n > 0 {
+			fmt.Println(message)
+		}
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
 }
 
 func main() {
