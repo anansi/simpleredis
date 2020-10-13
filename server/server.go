4ed238263ac91d398cfbb0745d4c0ef7cdf1910b
Julian Hulme <julian@arcadesoftware.io>
2020-10-13 18:29:17 +0200

    the initial stage of a net server. making the app stay alive!

--- server/server.go
+++ server/server.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	fmt.Println("handling Connection")
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	fmt.Println("Hello, World!")
+
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		// handle error
+		fmt.Println("// handle error for ln")
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+			fmt.Println("// handle error for conn")
+		}
+		go handleConnection(conn)
+	}
+
 }
