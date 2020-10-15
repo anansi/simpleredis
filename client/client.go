b48266223bf71e575f1f0c880a8b125ee52208bc
Julian Hulme <julian@arcadesoftware.io>
2020-10-15 10:55:18 +0200

    added cmd line args checks for server

--- client/client.go
+++ client/client.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"simpleredis.task/clientapi"
 	"os"
+	"log"
 )
 
 func main() {
 
-	// access command line parameters
-	cmd := os.Args[1]
-	params := os.Args[2:]
+	// Allow command line usage, check for valid arguments
+	if len(os.Args) == 1	{
+		log.Println("Error: Provided at least 1 cmd line argument: SET or GET")
+		os.Exit(1)
+	}
 
 	// call the api to contact the server
 	var response = clientapi.ExecuteCmd(cmd, params)
