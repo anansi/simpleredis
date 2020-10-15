d85b9b6b3943085f7242a527028fc698b3106805
Julian Hulme <julian@arcadesoftware.io>
2020-10-15 10:56:41 +0200

    better layout of client api module

--- clientapi/clientapi.go
+++ clientapi/clientapi.go
@@ -1,32 +1,19 @@
 package clientapi
 
 import (
+	"os"
+	"log"
 	"net"
 	"strings"
 	"simpleredis.task/coder"
 )
 
-const get = "GET"
-const set = "SET"
-
-// sendNetworkRequest is a private function for sending data to the server
-func sendNetworkRequest(data []byte) string {
-	conn, err := net.Dial("tcp", "localhost:5566")
-	defer conn.Close()
-
-	if err != nil {
-		// handle error
-	}
-
-	// write the data to the connection
-	_, err = conn.Write(data)
-
-	reply := make([]byte, 2048)
-	conn.Read(reply)
-
-	return string(reply)
-
-}
+const (
+	get = "GET"
+	set = "SET"
+	serverIpPort = "localhost:5566"
+	networkProtocol = "tcp"
+)
 
 // ExeuteCmd allows a client to execute any command supplied with its parameters.
 // It returns the relivant response from the API call 
@@ -34,12 +21,23 @@ func ExecuteCmd(cmd string, params []string) string {
 
 	switch strings.ToUpper(cmd) { // ToUpper makes the user input case insensitive
 	case "GET":
+		if len(params) != 1	{
+			log.Printf("Error: GET cmd requires 1 parameter, but %d were provided. exiting",len(params))
+			os.Exit(1)
+		}
 		return Get(params[0])
 	case "SET":
+		if len(params) != 2	{
+			log.Println("Error: SET cmd requires 2 parameters, but",len(params),"parameter(s) were provided. exiting")
+			os.Exit(1)
+		}
 		return Set(params[0], params[1])
+	
 	}
 
-	return "provided unsupported command"
+	log.Println("Error: Provided unsupported command: ", strings.ToUpper(cmd))
+	os.Exit(1)
+	return ""// Go requires me to end this function with a return. 
 }
 
 // Get allows a Client to ask the server to retreive the value of a key in the simpleredis datastore
@@ -55,14 +53,19 @@ func Get(key string) string {
 
 	// TODO send the command to the server
 	// TODO get the response from the server
-	response := sendNetworkRequest(concat)
+	responseData := sendNetworkRequest(concat)
+	var response []string = coder.Decode(responseData)
 
 	// TODO return the value obtained, as required
-	return response
-
+	if len(response) == 0	{
+		// a known response from the README.md protocol, is an empty string (no word)
+		return ""
+	}	else	{
+		return response[0]
+	}
 }
 
-// Set lets the client set a value in the simpleredis datastore
+// Set lets the client set a new value in the simpleredis datastore
 // Set is made Public, for convinience for the client
 func Set(key, value string) string {
 
@@ -78,6 +81,27 @@ func Set(key, value string) string {
 	response := sendNetworkRequest(concat)
 
 	// TODO return the value obtained, as required
-	return response
+	strResponse := coder.Decode(response)[0]
+	return strResponse
+
+}
+
+// sendNetworkRequest is a private function for sending data to the server
+func sendNetworkRequest(data []byte) []byte {
+	conn, err := net.Dial(networkProtocol, serverIpPort)
+	if err != nil {
+		// handle error
+		log.Printf("Error: Server is not running, is it running at %s?",serverIpPort)
+		os.Exit(1)
+	}
+	defer conn.Close() // final call of this function
+
+	// write the data to the connection
+	_, err = conn.Write(data)
+
+	replyData := make([]byte, 2048)
+	conn.Read(replyData)
+
+	return replyData
 
 }
