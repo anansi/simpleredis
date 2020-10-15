1ad6c66c3b90020e82e418584fab1909fbbaf34b
Julian Hulme <julian@arcadesoftware.io>
2020-10-15 14:21:44 +0200

    added commented code that can allow testing for high access concurrency testing

--- client/client.go
+++ client/client.go
@@ -22,4 +22,21 @@ func main() {
 	var response = clientapi.ExecuteCmd(cmd, params)
 	fmt.Println(response)
 
+
+	// Uncomment the code below to spam the server, for easy concurrency testing
+	// Usage: pass valid arguments to the client in terminal, and observe an infinitely loop of SET and GET. Run the client like this on multiple terminals to create concurrency
+
+	// //BEGIN SPAM TEST BLOCK
+	// cnt := 0
+	// for {
+	// 	updatingSpamValue := string(cnt) + "__there"
+	// 	// call the clientapi to contact the server
+	// 	var response = clientapi.Set("hey", updatingSpamValue)
+	// 	fmt.Println(response)
+	// 	var response2 = clientapi.Get("hey")		
+	// 	fmt.Println(response2)
+	// 	cnt += 1
+	// }
+	// //END SPAM TEST BLOCK
+
 }
